f91345d520873c1566d02d21a3f9cf54e4ff3c23
filipecamargos <filipe.atrabalho@gmail.com>
2022-02-08 20:52:19 -0700

    Dummy data ready for card

---
+++ src/components/Cards/RestaurantCard.js
@@ -0,0 +1,17 @@
+const DUMMY_DATA = {
+    name: "Restaurante Name",
+    priceTag: '$$',
+    review: 2,
+    numberOfReviews: 100,
+    address: "123 Random St. Rexburg, ID 83440",
+    site: "www.restaurant.whatever",
+    phone: "(123) 456 -7890",
+    type: ['Burgers', 'American'],
+}
+
+const RestaurantCard = (props) => {
+    return <h1>Test</h1>
+  
+};
+  
+export default RestaurantCard;
