40ad501bf336186d6bd5c29e4d5ee6e4b48fc7e4
filipecamargos <filipe.atrabalho@gmail.com>
2022-02-11 15:58:32 -0700

    Card more dynamic and easier to implement on Home

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,11 +1,27 @@
-import { Link } from "react-router-dom";
 import RestaurantCard from "../Cards/RestaurantCard/RestaurantCard";
+
+//TODO: Remove the DUMMY_DATA and get it from the props!
+const RESTAURANT_DATA = {
+    "address": "615 E Iona Rd, Idaho Falls",
+    "city": "Idaho Falls",
+    "id": "ChIJa0ULuUBcVFMRjhzFvzz2TcY",
+    "image": "http://tny.im/rdJ",
+    "name": "Mitchell's Restaurant",
+    "phone": "(208) 525-8834",
+    "price_range": "$",
+    "rating": 3.5,
+    "type": [
+    "American",
+    "Burgers",
+    "Fries",
+    "Steak"
+    ],
+    "website": "https://www.mitchellsrestaurant-idaho.com/"
+};
 
 const Home = () => {
   return <div>
-      <Link  to={'restaurant/' + 'id_of_the_restaurante'} key={'id_of_the_restaurante'}>
-        <RestaurantCard />
-      </Link>
+        <RestaurantCard restaurante={RESTAURANT_DATA}/>
     </div>
 };
 
