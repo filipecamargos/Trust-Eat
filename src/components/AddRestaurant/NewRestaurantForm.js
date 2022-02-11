3c69ceb0db6b6c09d8ee3bec5a69b6b185ec9240
Yu-Chun Tung <dyj0183@byui.edu>
2022-02-10 19:40:40 -0700

    added error message for all the required input fields

--- src/components/AddRestaurant/NewRestaurantForm.js
+++ src/components/AddRestaurant/NewRestaurantForm.js
@@ -1,9 +1,9 @@
 import React from "react";
-// useFormInput is a customized hook to handle input error validation
+// useFormInput is a customized hook to handle input's value and error
 import useFormInput from "../../hooks/useFormInput";
 
 const NewRestaurantForm = () => {
-	// handle name field error validation
+	// handle name field
 	const {
 		value: enteredNameValue,
 		isValid: enteredNameIsValid,
@@ -13,7 +13,7 @@ const NewRestaurantForm = () => {
 		reset: resetNameInput,
 	} = useFormInput((value) => value.trim() !== "");
 
-	// handle address field error validation
+	// handle address field
 	const {
 		value: enteredAddressValue,
 		isValid: enteredAddressIsValid,
@@ -23,11 +23,66 @@ const NewRestaurantForm = () => {
 		reset: resetAddressInput,
 	} = useFormInput((value) => value.trim() !== "");
 
+	// handle phone number field
+	const {
+		value: enteredPhoneValue,
+		isValid: enteredPhoneIsValid,
+		hasError: phoneInputHasError,
+		valueChangeHandler: phoneChangedHandler,
+		inputBlurHandler: phoneBlurHandler,
+		reset: resetPhoneInput,
+	} = useFormInput((value) => value.trim() !== ""); // will add regex logic later
+
+	// handle types of food field
+	const {
+		value: enteredTypeValue,
+		isValid: enteredTypeIsValid,
+		hasError: typeInputHasError,
+		valueChangeHandler: typeChangedHandler,
+		inputBlurHandler: typeBlurHandler,
+		reset: resetTypeInput,
+	} = useFormInput((value) => value.trim() !== "");
+
+	// handle price range field
+	const {
+		value: enteredPriceValue,
+		// isValid: enteredPriceIsValid,
+		// hasError: priceInputHasError,
+		valueChangeHandler: priceChangedHandler,
+		inputBlurHandler: priceBlurHandler,
+		reset: resetPriceInput,
+	} = useFormInput((value) => value.trim() !== "");
+
+	// handle website field (optional field)
+	const {
+		value: enteredWebsiteValue,
+		// isValid: enteredWebsiteIsValid,
+		// hasError: websiteInputHasError,
+		valueChangeHandler: websiteChangedHandler,
+		inputBlurHandler: websiteBlurHandler,
+		reset: resetWebsiteInput,
+	} = useFormInput(() => {});
+
+	// handle url to image field (optional field)
+	const {
+		value: enteredImageUrlValue,
+		// isValid: enteredImageUrlIsValid,
+		// hasError: imageUrlInputHasError,
+		valueChangeHandler: imageUrlChangedHandler,
+		inputBlurHandler: imageUrlBlurHandler,
+		reset: resetImageUrlInput,
+	} = useFormInput(() => {});
+
 	// when the page just loads, set initial value to false
 	let formIsValid = false;
 
 	// check all the input fields to see if the entire form is valid or not
-	if (enteredNameIsValid) {
+	if (
+		enteredNameIsValid &&
+		enteredAddressIsValid &&
+		enteredPhoneIsValid &&
+		enteredTypeIsValid
+	) {
 		formIsValid = true;
 	}
 
@@ -43,6 +98,12 @@ const NewRestaurantForm = () => {
 
 		// after we process all the data from submission, clean up all the input fields
 		resetNameInput();
+		resetAddressInput();
+		resetPhoneInput();
+		resetTypeInput();
+		resetPriceInput("$");
+		resetWebsiteInput();
+		resetImageUrlInput();
 	};
 
 	// set up dynamics class to display error message
@@ -85,55 +146,82 @@ const NewRestaurantForm = () => {
 			<div className="">
 				<label htmlFor="phone">Phone Number*</label>
 				<br />
-				<input type="text" id="phone" value="" onChange="" onBlur="" />
-				{/* {phoneHasError && (
+				<input
+					type="text"
+					id="phone"
+					value={enteredPhoneValue}
+					onChange={phoneChangedHandler}
+					onBlur={phoneBlurHandler}
+				/>
+				{phoneInputHasError && (
 					<p className="error-text">Please enter a valid phone number.</p>
-				)} */}
+				)}
 			</div>
 			<div className="">
 				<label htmlFor="type">Types of Food (separated by commas)*</label>
 				<br />
-				<input type="text" id="type" value="" onChange="" onBlur="" />
+				<input
+					type="text"
+					id="type"
+					value={enteredTypeValue}
+					onChange={typeChangedHandler}
+					onBlur={typeBlurHandler}
+				/>
 				<p>Ex: American, Burgers, Steak...</p>
-				{/* {typeHasError && (
-					<p className="error-text">Please enter at least one type for this restaurant.</p>
-				)} */}
+				{typeInputHasError && (
+					<p className="error-text">
+						Please enter at least one type for this restaurant.
+					</p>
+				)}
 			</div>
 			<div className="">
 				<label htmlFor="price_range">Price Range</label>
 				<br />
-				<select id="price_range" value="" onChange="" onBlur="">
+				<select
+					id="price_range"
+					value={enteredPriceValue}
+					onChange={priceChangedHandler}
+					onBlur={priceBlurHandler}
+				>
 					<option>$</option>
 					<option>$$</option>
 					<option>$$$</option>
 					<option>$$$$</option>
 				</select>
-				{/* {priceRangeHasError && (
+				{/* {priceInputHasError && (
 					<p className="error-text">Please select a price range.</p>
 				)} */}
 			</div>
 			<div className="">
 				<label htmlFor="website">Website (Optional)</label>
 				<br />
-				<input type="text" id="website" value="" onChange="" onBlur="" />
-				{/* {websiteHasError && (
+				<input
+					type="text"
+					id="website"
+					value={enteredWebsiteValue}
+					onChange={websiteChangedHandler}
+					onBlur={websiteBlurHandler}
+				/>
+				{/* {websiteInputHasError && (
 					<p className="error-text">Please enter a valid website.</p>
 				)} */}
 			</div>
 			<div className="">
 				<label htmlFor="image">Url to Image (Optional)</label>
 				<br />
-				<p>
-					Find an image of the restaurant on Google, right click to copy image
-					url and paste here
-				</p>
-				<input type="text" id="image" value="" onChange="" onBlur="" />
-				{/* {imageUrlHasError && (
+				<input
+					type="text"
+					id="image"
+					value={enteredImageUrlValue}
+					onChange={imageUrlChangedHandler}
+					onBlur={imageUrlBlurHandler}
+				/>
+				{/* {imageUrlInputHasError && (
 					<p className="error-text">Please enter an image url for this restaurant.</p>
 				)} */}
 			</div>
 			<div className="form-actions">
-				<button disabled={!formIsValid}>Submit</button>
+				<button disabled={!formIsValid}>Add New Restaurant</button>
 			</div>
 		</form>
 	);
