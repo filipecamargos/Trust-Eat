667b7846423f0d011afe1d257b10dd73959fafd8
Yu-Chun Tung <dyj0183@byui.edu>
2022-02-10 19:16:59 -0700

    changed order of the input fields and set up error validatin for address

--- src/components/AddRestaurant/NewRestaurantForm.js
+++ src/components/AddRestaurant/NewRestaurantForm.js
@@ -1,9 +1,9 @@
 import React from "react";
-// useFormInput is a customized hook to handle form error validation
+// useFormInput is a customized hook to handle input error validation
 import useFormInput from "../../hooks/useFormInput";
 
 const NewRestaurantForm = () => {
-	// pass name rule to the hook to handle form validation
+	// handle name field error validation
 	const {
 		value: enteredNameValue,
 		isValid: enteredNameIsValid,
@@ -13,13 +13,27 @@ const NewRestaurantForm = () => {
 		reset: resetNameInput,
 	} = useFormInput((value) => value.trim() !== "");
 
+	// handle address field error validation
+	const {
+		value: enteredAddressValue,
+		isValid: enteredAddressIsValid,
+		hasError: addressInputHasError,
+		valueChangeHandler: addressChangedHandler,
+		inputBlurHandler: addressBlurHandler,
+		reset: resetAddressInput,
+	} = useFormInput((value) => value.trim() !== "");
+
+	// when the page just loads, set initial value to false
 	let formIsValid = false;
 
+	// check all the input fields to see if the entire form is valid or not
 	if (enteredNameIsValid) {
 		formIsValid = true;
 	}
 
+	// this function will be trigerred when the users click the submit button
 	const submitHandler = (event) => {
+		// prevent default behavior of the browser
 		event.preventDefault();
 
 		// if any field is missing or doesn't meet the requirement, we don't keep going to process the data
@@ -27,9 +41,11 @@ const NewRestaurantForm = () => {
 			return;
 		}
 
-        resetNameInput();
+		// after we process all the data from submission, clean up all the input fields
+		resetNameInput();
 	};
 
+	// set up dynamics class to display error message
 	const nameClasses = nameInputHasError
 		? "form-control invalid"
 		: "form-control";
@@ -37,23 +53,37 @@ const NewRestaurantForm = () => {
 	return (
 		<form onSubmit={submitHandler}>
 			<div className={nameClasses}>
-				<label htmlFor="name">Name</label>
+				<label htmlFor="name">Restaurant Name*</label>
 				<br />
-				<input type="text" id="name" value={enteredNameValue} onChange={nameChangedHandler} onBlur={nameBlurHandler} />
+				<input
+					type="text"
+					id="name"
+					value={enteredNameValue}
+					onChange={nameChangedHandler}
+					onBlur={nameBlurHandler}
+				/>
 				{nameInputHasError && (
 					<p className="error-text">Please enter a restaurant name.</p>
 				)}
 			</div>
 			<div className="">
-				<label htmlFor="address">Address</label>
+				<label htmlFor="address">Address*</label>
 				<br />
-				<input type="text" id="address" value="" onChange="" onBlur="" />
-				{/* {addressHasError && (
-					<p className="error-text">Please enter an address for this restaurant.</p>
-				)} */}
+				<input
+					type="text"
+					id="address"
+					value={enteredAddressValue}
+					onChange={addressChangedHandler}
+					onBlur={addressBlurHandler}
+				/>
+				{addressInputHasError && (
+					<p className="error-text">
+						Please enter an address for this restaurant.
+					</p>
+				)}
 			</div>
 			<div className="">
-				<label htmlFor="phone">Phone</label>
+				<label htmlFor="phone">Phone Number*</label>
 				<br />
 				<input type="text" id="phone" value="" onChange="" onBlur="" />
 				{/* {phoneHasError && (
@@ -61,32 +91,12 @@ const NewRestaurantForm = () => {
 				)} */}
 			</div>
 			<div className="">
-				<label htmlFor="website">Website</label>
+				<label htmlFor="type">Types of Food (separated by commas)*</label>
 				<br />
-				<input type="text" id="website" value="" onChange="" onBlur="" />
-				{/* {websiteHasError && (
-					<p className="error-text">Please enter a valid website.</p>
-				)} */}
-			</div>
-			<div className="">
-				<label htmlFor="type">Type</label>
-				<br />
-				<p>Ex: American, Burgers, Steak... (Please seperate by comma)</p>
 				<input type="text" id="type" value="" onChange="" onBlur="" />
+				<p>Ex: American, Burgers, Steak...</p>
 				{/* {typeHasError && (
 					<p className="error-text">Please enter at least one type for this restaurant.</p>
-				)} */}
-			</div>
-			<div className="">
-				<label htmlFor="image">Image Url</label>
-				<br />
-				<p>
-					Find an image of the restaurant on Google, right click to copy image
-					url and paste here
-				</p>
-				<input type="text" id="image" value="" onChange="" onBlur="" />
-				{/* {imageUrlHasError && (
-					<p className="error-text">Please enter an image url for this restaurant.</p>
 				)} */}
 			</div>
 			<div className="">
@@ -103,22 +113,27 @@ const NewRestaurantForm = () => {
 				)} */}
 			</div>
 			<div className="">
-				<label htmlFor="rating">Rating (5 means the Best)</label>
+				<label htmlFor="website">Website (Optional)</label>
 				<br />
-				<select id="rating" value="" onChange="" onBlur="">
-					<option>5</option>
-					<option>4</option>
-					<option>3</option>
-					<option>2</option>
-					<option>1</option>
-				</select>
-				{/* {ratingHasError && (
-					<p className="error-text">Please select a rating.</p>
+				<input type="text" id="website" value="" onChange="" onBlur="" />
+				{/* {websiteHasError && (
+					<p className="error-text">Please enter a valid website.</p>
 				)} */}
 			</div>
-
+			<div className="">
+				<label htmlFor="image">Url to Image (Optional)</label>
+				<br />
+				<p>
+					Find an image of the restaurant on Google, right click to copy image
+					url and paste here
+				</p>
+				<input type="text" id="image" value="" onChange="" onBlur="" />
+				{/* {imageUrlHasError && (
+					<p className="error-text">Please enter an image url for this restaurant.</p>
+				)} */}
+			</div>
 			<div className="form-actions">
-				<button disabled>Submit</button>
+				<button disabled={!formIsValid}>Submit</button>
 			</div>
 		</form>
 	);
