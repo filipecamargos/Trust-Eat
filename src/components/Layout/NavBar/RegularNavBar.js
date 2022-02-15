ffba4b08da4f5a2a05f13e0095d518e29e71d409
Tejada-Joyce <joyce_105_3@hotmail.com>
2022-02-14 23:56:45 -0700

    changed logo and adjusted search width

--- src/components/Layout/NavBar/RegularNavBar.js
+++ src/components/Layout/NavBar/RegularNavBar.js
@@ -1,22 +1,22 @@
 import { Navbar, Nav, Container } from "react-bootstrap";
 import { LinkContainer } from "react-router-bootstrap";
 import classes from "./RegularNavBar.module.css";
-import logo from "../../../images/logo192.png";
+import logo from "../../../images/review-app-logo.png";
 
 const MainNavBar = () => {
   return (
     <Navbar expand="sm" bg="dark" variant="dark">
       <Container fluid>
         <LinkContainer to="/">
-          <Navbar.Brand>
+          <Navbar.Brand style={{ display: "flex", alignItems: "center" }}>
             <img
               alt=""
               src={logo}
-              width="30"
-              height="30"
+              width="40"
+              height="40"
               className="d-inline-block align-top"
-            />{" "}
-            <span>Cool App Name</span>
+            />
+            <span style={{ marginLeft: "10px" }}>BYU-I Trust Eat</span>
           </Navbar.Brand>
         </LinkContainer>
         <Navbar.Toggle
@@ -25,7 +25,10 @@ const MainNavBar = () => {
         >
           <i className="fa fa-angle-down" style={{ fontSize: 24 }}></i>
         </Navbar.Toggle>
-        <Navbar.Collapse id="responsive-navbar-nav">
+        <Navbar.Collapse
+          id="responsive-navbar-nav"
+          className={classes.collapse}
+        >
           <LinkContainer to="/">
             <Nav.Link className={classes.hidden}>Home</Nav.Link>
           </LinkContainer>
