b0e2643926eed273f0a20aca746479fab6bba701
Yu-Chun Tung <dyj0183@byui.edu>
2022-02-16 04:55:26 -0700

    use fetch to POST new review to firebase

--- src/components/AddReview/AddReview.js
+++ src/components/AddReview/AddReview.js
@@ -5,16 +5,56 @@ import NewReviewForm from "./NewReviewForm";
 const AddReview = (props) => {
 	// when the users click on a restaurant, we will receive the id of the restaurant
 	const restaurant_id = props.restaurantId;
-	console.log(restaurant_id);
 
 	const submitReviewHandler = (newReviewData) => {
-        console.log(newReviewData);
-    };
+		const reviewData = {
+			restaurant_id: restaurant_id,
+			...newReviewData,
+		};
+
+		addReviewHandler(reviewData);
+	};
+
+	// send the data to firebase to add a new restaurant to Rexburg list
+	const addReviewHandler = async (newReviewData) => {
+		try {
+			const response = await fetch(
+				"https://react-restaurant-review-app-default-rtdb.firebaseio.com/reviews.json",
+				{
+					method: "POST",
+					body: JSON.stringify(newReviewData),
+					headers: {
+						"Content-Type": "application/json",
+					},
+				}
+			);
+
+			// handle the error
+			if (!response.ok) {
+				throw new Error(
+					"Something went wrong. Please contact the food app team at test@gmail.com"
+				);
+			} else {
+				// if the response is okay, then we want to proceed to update the num_of_reviews and rating for the restaurant
+				// data is a JavaScript object
+				const data = await response.json();
+				console.log(data.name);
+				fetch(
+					"https://react-restaurant-review-app-default-rtdb.firebaseio.com/restaurants.json"
+				);
+			}
+		} catch (err) {
+			console.log(err);
+		}
+	};
 
 	return (
 		<div className="container">
 			<h1 className="header">Write New Review for {props.restaurantName}</h1>
-			<NewReviewForm onSubmitReview={submitReviewHandler} onCloseModal={props.onCloseModal}></NewReviewForm>
+			<NewReviewForm
+				onSubmitReview={submitReviewHandler}
+				onCloseModal={props.onCloseModal}
+			></NewReviewForm>
 		</div>
 	);
 };
